a268c325bdef1c1c65444d82090ecf1a026b522a
Ntando Magana <ntandomagana@gmail.com>
2025-01-20 21:54:11 +0200

    quizmodel

--- src/main/java/org/example/QuizModel.java
+++ src/main/java/org/example/QuizModel.java
@@ -2,4 +2,28 @@ package org.example;
 
 public class QuizModel {
 
+    private final String question;
+    private final String[] options;
+    private final String correctAnswer;
+
+
+    public QuizModel(String question, String[] options, String correctAnswer) {
+        this.question = question;
+        this.options = options;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+
 }
