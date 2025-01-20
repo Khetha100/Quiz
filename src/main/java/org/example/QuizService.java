64a06da45e1662330cf45dc9edd4b56cd8fe5e4e
Khetha100 <khethukuthula990@gmail.com>
2025-01-20 19:59:13 +0200

    initial quiz structure

---
+++ src/main/java/org/example/QuizService.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public interface QuizService {
+    public void startQuiz();
+
+    public String getUserInput();
+
+    public void trackScore();
+
+    public String getCurrentQuestion();
+
+}
